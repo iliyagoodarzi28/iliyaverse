648ac9943c9c037f9da8dca0120d5fc016d89e18
ILIYA <igoodarzi4@gmail.com>
2025-03-26 00:27:36 -0700

    a

--- develop/settings.py
+++ develop/settings.py
@@ -83,13 +83,13 @@ WSGI_APPLICATION = 'develop.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': '/tmp/db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',  # مسیر کامل فایل SQLite
     }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
