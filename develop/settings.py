160b800c46197c3c1c13d7b5030dcafb352f8153
ILIYA <igoodarzi4@gmail.com>
2025-03-26 00:09:04 -0700

    a

--- develop/settings.py
+++ develop/settings.py
@@ -83,13 +83,17 @@ WSGI_APPLICATION = 'develop.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'your_database_name',
+        'USER': 'your_database_user',
+        'PASSWORD': 'your_database_password',
+        'HOST': 'localhost',  # یا آدرس سرور پایگاه داده
+        'PORT': '80',       # پورت پیش‌فرض PostgreSQL
     }
 }
+
 
 
 # Password validation
